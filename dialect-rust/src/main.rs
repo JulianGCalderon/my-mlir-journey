ab653dee439409f4bed6743778e0b69828293504
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-08-28 16:04:17 -0300

    Initialize context

--- dialect-rust/src/main.rs
+++ dialect-rust/src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+use melior::{
+    Context,
+    dialect::DialectRegistry,
+    utility::{register_all_dialects, register_all_llvm_translations, register_all_passes},
+};
+
+fn main() {
+    let context = initialize_context();
+}
+
+fn initialize_context() -> Context {
+    let context = Context::new();
+    context.append_dialect_registry(&{
+        let registry = DialectRegistry::new();
+        register_all_dialects(&registry);
+        registry
+    });
+    context.load_all_available_dialects();
+    register_all_passes();
+    register_all_llvm_translations(&context);
+    context
+}
