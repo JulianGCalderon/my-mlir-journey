508a6e57725216ced6f034287612c018faac7a9d
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-08-29 11:08:20 -0300

    Minor improvements to pdl module

--- dialect-rust/src/pdl.rs
+++ dialect-rust/src/pdl.rs
@@ -13,9 +13,40 @@ use mlir_sys::{
     mlirPDLAttributeTypeGet, mlirPDLOperationTypeGet, mlirPDLTypeTypeGet, mlirPDLValueTypeGet,
 };
 
+pub fn load_pattern_module(ctx: &'_ Context) -> Module<'_> {
+    Module::parse(
+        ctx,
+        r#"
+        module {
+          pdl.pattern : benefit(1) {
+            %0 = type
+            %1 = operand
+            %2 = operand
+            %3 = operation "felt.add"(%1, %2 : !pdl.value, !pdl.value)  -> (%0 : !pdl.type)
+            rewrite %3 {
+              %4 = operation "arith.addi"(%1, %2 : !pdl.value, !pdl.value)  -> (%0 : !pdl.type)
+              %5 = result 0 of %4
+              %6 = attribute = 13 : i32
+              %7 = operation "arith.constant"  {"value" = %6} -> (%0 : !pdl.type)
+              %8 = result 0 of %7
+              %9 = operation "arith.remui"(%5, %8 : !pdl.value, !pdl.value)  -> (%0 : !pdl.type)
+              replace %3 with %9
+            }
+          }
+        }"#,
+    )
+    .unwrap()
+}
+
 pub fn build_pattern_module(ctx: &'_ Context) -> Module<'_> {
     let location = Location::unknown(ctx);
     let module = Module::new(location);
+
+    let pdl_type_type = unsafe { Type::from_raw(mlirPDLTypeTypeGet(ctx.to_raw())) };
+    let pdl_value_type = unsafe { Type::from_raw(mlirPDLValueTypeGet(ctx.to_raw())) };
+    let pdl_attribute_type = unsafe { Type::from_raw(mlirPDLAttributeTypeGet(ctx.to_raw())) };
+    let pdl_operation_type = unsafe { Type::from_raw(mlirPDLOperationTypeGet(ctx.to_raw())) };
+    let u32_type: Type<'_> = IntegerType::new(ctx, 32).into();
 
     module.body().append_operation(
         melior::dialect::ods::pdl::PatternOperation::builder(ctx, location)
@@ -23,13 +54,6 @@ pub fn build_pattern_module(ctx: &'_ Context) -> Module<'_> {
             .body_region({
                 let region = Region::new();
                 let block = region.append_block(Block::new(&[]));
-
-                let pdl_type_type = unsafe { Type::from_raw(mlirPDLTypeTypeGet(ctx.to_raw())) };
-                let pdl_value_type = unsafe { Type::from_raw(mlirPDLValueTypeGet(ctx.to_raw())) };
-                let pdl_attribute_type =
-                    unsafe { Type::from_raw(mlirPDLAttributeTypeGet(ctx.to_raw())) };
-                let pdl_operation_type =
-                    unsafe { Type::from_raw(mlirPDLOperationTypeGet(ctx.to_raw())) };
 
                 let result = block
                     .append_op_result(pdl::r#type(ctx, pdl_type_type, location).into())
@@ -76,7 +100,7 @@ pub fn build_pattern_module(ctx: &'_ Context) -> Module<'_> {
                             let region = Region::new();
                             let block = region.append_block(Block::new(&[]));
 
-                            let addi_operation = block
+                            let add_operation = block
                                 .append_op_result(
                                     OperationBuilder::new("pdl.operation", location)
                                         .add_operands(&[operand1, operand2, result])
@@ -100,17 +124,13 @@ pub fn build_pattern_module(ctx: &'_ Context) -> Module<'_> {
                                 )
                                 .unwrap();
 
-                            let addi_result = block
+                            let add_result = block
                                 .append_op_result(
                                     OperationBuilder::new("pdl.result", location)
-                                        .add_operands(&[addi_operation])
+                                        .add_operands(&[add_operation])
                                         .add_attributes(&[(
                                             Identifier::new(ctx, "index"),
-                                            IntegerAttribute::new(
-                                                IntegerType::new(ctx, 32).into(),
-                                                0,
-                                            )
-                                            .into(),
+                                            IntegerAttribute::new(u32_type, 0).into(),
                                         )])
                                         .add_results(&[pdl_value_type])
                                         .build()
@@ -123,11 +143,7 @@ pub fn build_pattern_module(ctx: &'_ Context) -> Module<'_> {
                                     OperationBuilder::new("pdl.attribute", location)
                                         .add_attributes(&[(
                                             Identifier::new(ctx, "value"),
-                                            IntegerAttribute::new(
-                                                IntegerType::new(ctx, 32).into(),
-                                                13,
-                                            )
-                                            .into(),
+                                            IntegerAttribute::new(u32_type, 13).into(),
                                         )])
                                         .add_results(&[pdl_attribute_type])
                                         .build()
@@ -163,17 +179,13 @@ pub fn build_pattern_module(ctx: &'_ Context) -> Module<'_> {
                                 )
                                 .unwrap();
 
-                            let k13 = block
+                            let k13_result = block
                                 .append_op_result(
                                     OperationBuilder::new("pdl.result", location)
                                         .add_operands(&[k13_operation])
                                         .add_attributes(&[(
                                             Identifier::new(ctx, "index"),
-                                            IntegerAttribute::new(
-                                                IntegerType::new(ctx, 32).into(),
-                                                0,
-                                            )
-                                            .into(),
+                                            IntegerAttribute::new(u32_type, 0).into(),
                                         )])
                                         .add_results(&[pdl_value_type])
                                         .build()
@@ -181,10 +193,10 @@ pub fn build_pattern_module(ctx: &'_ Context) -> Module<'_> {
                                 )
                                 .unwrap();
 
-                            let remui_operation = block
+                            let modulo_operation = block
                                 .append_op_result(
                                     OperationBuilder::new("pdl.operation", location)
-                                        .add_operands(&[addi_result, k13, result])
+                                        .add_operands(&[add_result, k13_result, result])
                                         .add_attributes(&[
                                             (
                                                 Identifier::new(ctx, "opName"),
@@ -207,7 +219,7 @@ pub fn build_pattern_module(ctx: &'_ Context) -> Module<'_> {
 
                             block.append_operation(
                                 OperationBuilder::new("pdl.replace", location)
-                                    .add_operands(&[operation, remui_operation])
+                                    .add_operands(&[operation, modulo_operation])
                                     .add_attributes(&[(
                                         Identifier::new(ctx, "operandSegmentSizes"),
                                         DenseI32ArrayAttribute::new(ctx, &[1, 1, 0]).into(),
@@ -229,4 +241,23 @@ pub fn build_pattern_module(ctx: &'_ Context) -> Module<'_> {
     );
 
     module
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        initialize_context,
+        pdl::{build_pattern_module, load_pattern_module},
+    };
+
+    #[test]
+    fn equal_load_and_build() {
+        let context = initialize_context();
+        let builded_module = build_pattern_module(&context);
+        let loaded_module = load_pattern_module(&context);
+        assert_eq!(
+            builded_module.as_operation().to_string(),
+            loaded_module.as_operation().to_string()
+        )
+    }
 }
